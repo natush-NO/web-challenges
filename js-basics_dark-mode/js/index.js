d77965e5fe2f110fd2325df4d81119eda22bd4dc
Nataliia Osman <osmannatali22@gmail.com>
2024-07-02 14:53:46 +0200

    Changed the name of the variable for the last button

--- js-basics_dark-mode/js/index.js
+++ js-basics_dark-mode/js/index.js
@@ -4,7 +4,7 @@ const bodyElement = document.querySelector('[data-js="body"]');
 
 const darkButton = document.querySelector("[data-js='dark-mode-button']");
 const removeButton = document.querySelector("[data-js='light-mode-button']");
-const togglerButton = document.querySelector('[data-js="toggle-button"]');
+const toggleButton = document.querySelector('[data-js="toggle-button"]');
 
 darkButton.addEventListener("click", () => {
   bodyElement.classList.add("dark");
@@ -14,6 +14,6 @@ removeButton.addEventListener("click", () => {
   bodyElement.classList.remove("dark");
 });
 
-togglerButton.addEventListener("click", () => {
-  bodyElement.classList.add("dark");
+toggleButton.addEventListener("click", () => {
+  bodyElement.classList.toggle("dark");
 });
