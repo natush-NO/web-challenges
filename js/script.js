33ae8496331f8c4949b7ae0f085cc891f5953d8c
Nataliia Osman <osmannatali22@gmail.com>
2024-07-10 14:26:17 +0200

    Created a new folder

---
+++ js/script.js
@@ -0,0 +1,9 @@
+console.clear();
+
+const bookmark = document.querySelector("[data-js='book-mark-white']");
+
+bookmark;
+
+// bookmark.classList.add("card__bookmark-black");
+
+// bookmark.classList.add("card__bookmark-black");
