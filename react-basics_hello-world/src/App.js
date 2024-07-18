d7a93ae338d3b25a2428363d607a109e9692d4b0
Nataliia Osman <osmannatali22@gmail.com>
2024-07-18 15:08:59 +0200

    Completed the task

---
+++ react-basics_hello-world/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div>
+      <HelloWorld />
+    </div>
+  );
+}
+
+function HelloWorld() {
+  return <h1>Hello world!</h1>;
+}
