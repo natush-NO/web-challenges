438bd3daed60cf9a05db975761cae55f00c25135
Nataliia Osman <osmannatali22@gmail.com>
2024-07-21 22:06:53 +0200

    Remove deleted files

--- js/form.js
+++
@@ -1,9 +0,0 @@
-import { App } from "../Form.components/App/App.js";
-
-// The render function renders the app to the DOM
-function render() {
-  const root = document.querySelector("#root");
-  root.append(App());
-}
-
-render();
