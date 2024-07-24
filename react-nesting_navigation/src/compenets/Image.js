75f6e15b959587b934875435c1614dbec5786b04
Nataliia Osman <osmannatali22@gmail.com>
2024-07-24 10:51:50 +0200

    Commit

---
+++ react-nesting_navigation/src/compenets/Image.js
@@ -0,0 +1,9 @@
+function Image() {
+  return (
+    <>
+      <img className="round-image" src={logo} alt="logo" />
+      ;
+      <img className="round-image" src={avatar} alt="avatar" />
+    </>
+  );
+}
---
+++ react-nesting_navigation/src/compenets/Link.js
@@ -0,0 +1,15 @@
+function Link() {
+  return (
+    <>
+      <a className="navigation__link" href="#home">
+        Home
+      </a>
+      <a className="navigation__link" href="#about">
+        About
+      </a>
+      <a className="navigation__link" href="#impressum">
+        Impressum
+      </a>
+    </>
+  );
+}
