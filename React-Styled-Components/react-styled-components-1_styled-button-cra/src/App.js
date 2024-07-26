4060c00d40ed547512b92792545ac57213bf6694
Nataliia Osman <osmannatali22@gmail.com>
2024-07-26 12:02:13 +0200

    Completed the task

---
+++ React-Styled-Components/react-styled-components-1_styled-button-cra/src/App.js
@@ -0,0 +1,13 @@
+import StyledButton, { PrimaryButton } from "./components/StyledButton";
+
+function App() {
+  return (
+    <main>
+      <h1>Hello from React</h1>
+      <StyledButton>Default Button </StyledButton>
+      <PrimaryButton>Primary Button</PrimaryButton>
+    </main>
+  );
+}
+
+export default App;
